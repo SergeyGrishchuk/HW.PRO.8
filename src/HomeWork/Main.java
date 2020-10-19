268628671263353a8739d4510013da41ef6dc01e
Sergey <sirgeyaid666@gmail.com>
2020-10-19 19:42:06 +0300

    HomeWork

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -3,6 +3,6 @@ package HomeWork;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Спасибо за проведенный курс лекций!");
     }
 }
